05da1b69edc61537dcc003e693ade34aeb634477
Ronit <maity.ronit18@gmail.com>
2021-06-09 00:35:11 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+var compression = require('compression')
+
+const app = express();
+
+app.use(compression())
+
+
+
+app.use(express.static('./dist/durbaries'));
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: 'dist/durbaries'})
+})
+
+app.get('/events', function (req, res) {
+    res.setHeader('Content-Type', 'text/event-stream')
+    res.setHeader('Cache-Control', 'no-cache')
+   
+    
+    var timer = setInterval(function () {
+      res.write('data: ping\n\n')
+   
+     
+      res.flush()
+    }, 2000)
+   
+    res.on('close', function () {
+      clearInterval(timer)
+    })
+  })
+
+app.listen(process.env.PORT || 8080);
